2075c539610f42fafbfd4e436008fb182976bbcf
sindhujashaga173 <sindhujashaga173@gmail.com>
2026-02-18 16:52:22 -0600

    Define variables for instance types and S3 buckets
    Added variables for instance types, sizing, S3 bucket names, and security configurations.

--- submission/terraform/variables.tf
+++ submission/terraform/variables.tf
@@ -26,7 +26,64 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-# TODO: Add variables for:
-# - Node group instance types and sizing
-# - S3 bucket names
-# - Any other configurable parameters your infrastructure needs
+variable "general_instance_types" {
+  description = "Instance types for general workload node group"
+  type        = list(string)
+  default     = ["t3.large"]
+}
+
+variable "gpu_instance_types" {
+  description = "Instance types for GPU workload node group"
+  type        = list(string)
+  default     = ["g4dn.xlarge"]
+}
+
+variable "general_desired_size" {
+  type        = number
+  default     = 2
+}
+
+variable "general_min_size" {
+  type        = number
+  default     = 1
+}
+
+variable "general_max_size" {
+  type        = number
+  default     = 4
+}
+
+variable "gpu_desired_size" {
+  type        = number
+  default     = 1
+}
+
+variable "gpu_min_size" {
+  type        = number
+  default     = 0
+}
+
+variable "gpu_max_size" {
+  type        = number
+  default     = 2
+}
+
+# --- S3 Buckets ---
+
+variable "analytics_bucket_name" {
+  description = "S3 bucket for processed analytics"
+  type        = string
+}
+
+variable "video_chunks_bucket_name" {
+  description = "S3 bucket for raw uploaded video chunks"
+  type        = string
+}
+
+# --- Security ---
+
+variable "management_cidr" {
+  description = "CIDR block allowed to SSH into bastion"
+  type        = string
+}
+
